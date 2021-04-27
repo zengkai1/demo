04586c2e750292baca77856246bd8ed203272bd9
zengkai <18772100224@163.com>
2021-04-27 15:01:49 +0800

    新增list转tree工具类

---
+++ src/main/java/com/example/demo/util/List2TreeUtil.java
@@ -0,0 +1,59 @@
+package com.example.demo.util;
+
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+
+/**
+ * <p>
+ *   list 转 tree 工具类
+ * </p>
+ *
+ * @author: 曾凯
+ * @Version: V1.0
+ * @since:
+ */
+public class List2TreeUtil {
+    /**
+
+     - listToTree
+     - <p>方法说明<p>
+     - 将JSONArray数组转为树状结构
+     - @param arr 需要转化的数据
+     - @param id 数据唯一的标识键值
+     - @param pid 父id唯一标识键值
+     - @param child 子节点键值
+     - @return JSONArray
+     */
+    public static JSONArray listToTree(JSONArray arr, String id, String pid, String child){
+        JSONArray r = new JSONArray();
+        JSONObject hash = new JSONObject();
+        //将数组转为Object的形式，key为数组中的id
+        for(int i=0;i<arr.size();i++){
+            JSONObject json = (JSONObject) arr.get(i);
+            hash.put(json.getStr(id), json);
+        }
+        //遍历结果集
+        for(int j=0;j<arr.size();j++){
+            //单条记录
+            JSONObject aVal = (JSONObject) arr.get(j);
+            //在hash中取出key为单条记录中pid的值
+            JSONObject hashVP = (JSONObject) hash.get(aVal.get(pid).toString());
+            //如果记录的pid存在，则说明它有父节点，将她添加到孩子节点的集合中
+            if(hashVP!=null){
+                //检查是否有child属性
+                if(hashVP.get(child)!=null){
+                    JSONArray ch = (JSONArray) hashVP.get(child);
+                    ch.add(aVal);
+                    hashVP.put(child, ch);
+                }else{
+                    JSONArray ch = new JSONArray();
+                    ch.add(aVal);
+                    hashVP.put(child, ch);
+                }
+            }else{
+                r.add(aVal);
+            }
+        }
+        return r;
+    }
+}
---
+++ src/test/java/com/example/demo/List2TreeTest.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONUtil;
+import com.example.demo.util.List2TreeUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ *  list2Tree测试
+ * </p>
+ *
+ * @author: 曾凯
+ * @Version: V1.0
+ * @since:
+ */
+public class List2TreeTest {
+
+    public static void main(String[] args) {
+        List<Map<String,Object>> data = new ArrayList<>();
+        Map<String,Object> map = new HashMap<>();
+        map.put("id",0);
+        map.put("pid",1);
+        map.put("name","水果");
+        data.add(map);
+        Map<String,Object> map2 = new HashMap<>();
+        map2.put("id",1);
+        map2.put("pid",2);
+        map2.put("name","苹果");
+        data.add(map2);
+        Map<String,Object> map3 = new HashMap<>();
+        map3.put("id",3);
+        map3.put("pid",0);
+        map3.put("name","葡萄");
+        data.add(map3);
+        Map<String,Object> map4 = new HashMap<>();
+        map4.put("id",4);
+        map4.put("pid",0);
+        map4.put("name","哈密瓜");
+        data.add(map4);
+        Map<String,Object> map5 = new HashMap<>();
+        map5.put("id",5);
+        map5.put("pid",0);
+        map5.put("name","水蜜桃");
+        data.add(map5);
+        JSONArray result = List2TreeUtil.listToTree(JSONUtil.parseArray(JSONUtil.parse(data)),"id","pid","children");
+        System.out.println(JSONUtil.parse(result));
+    }
+}
